5c7495952f73718c4ec97eda47ab8c987ff05d60
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-09-24 21:44:14 +0300

    ready solution!

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -6,10 +7,47 @@ from taxi.models import Driver, Car, Manufacturer
 def index(request):
     """View function for the home page of the site."""
 
-    context = {
-        "num_drivers": Driver.objects.count(),
-        "num_cars": Car.objects.count(),
-        "num_manufacturers": Manufacturer.objects.count(),
-    }
+    return render(
+        request,
+        "taxi/index.html",
+        context={
+            "num_drivers": Driver.objects.count(),
+            "num_cars": Car.objects.count(),
+            "num_manufacturers": Manufacturer.objects.count(),
+        })
 
-    return render(request, "taxi/index.html", context=context)
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = (Car.objects.all().order_by("model")
+                .select_related("manufacturer"))
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["drivers"] = self.object.drivers.all()
+        return context
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cars"] = self.object.cars.all()
+        return context
